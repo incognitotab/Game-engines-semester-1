2d818b388c742ec8b6b0486634c943aca58c2a64
tabeeb choudhury <c17350031@mydit.ie>
2019-12-18 00:53:02 +0000

    second half of the project
    movements are more polished and added effects

--- New Unity Project/Assets/Scenes/soundvisualizer.cs
+++ New Unity Project/Assets/Scenes/soundvisualizer.cs
@@ -6,21 +6,24 @@ public class soundvisualizer : MonoBehaviour
 {
      AudioSource source;
     [SerializeField] Material dottedline;
-    public Camera backgroundeditor;
+    public Material backgroundeditor;
     public GameObject instantiator;
     public GameObject instantiatee;
-    int counter;
+    public GameObject instantiatortwo;
+    public GameObject instantiateetwo;
+   List<GameObject> points = new List<GameObject>(20);
    [SerializeField] List<GameObject> shapes = new List<GameObject>();
     List<Transform> sizes = new List<Transform>();
    [SerializeField] float[] soundsampling = new float[512];
+    Vector3[] angleset = new Vector3[20];
     Transform boxshape;
     Color32 rand;
     Color32 randbehind;
     bool test;
     bool reset;
     bool deathset;
-    float samplecode;
     bool finale;
+    float samplecode;
     float measure;
     float measureuse;
     // Start is called before the first frame update
@@ -41,12 +44,12 @@ public class soundvisualizer : MonoBehaviour
         {
             measure = soundsampling[1];
             measureuse = 250 - (measure * 100);
-            Debug.Log(measureuse);
+            
         }
             
         Vector3 setdet = new Vector3(soundsampling[1], soundsampling[1], soundsampling[1])*measureuse;
-        Vector3 retractor = Vector3.Lerp(boxshape.localScale, setdet, 0.5f);
-        Quaternion rotational = Quaternion.Lerp(boxshape.rotation, Quaternion.Euler(soundsampling[1]*1000, soundsampling[2]*1000, soundsampling[3]*1000),3);
+        Vector3 retractor = Vector3.Lerp(boxshape.localScale, setdet, 8f*Time.deltaTime);
+        Quaternion rotational = Quaternion.Lerp(boxshape.rotation, Quaternion.Euler(soundsampling[1]*1000, soundsampling[2]*1000, soundsampling[3]*1000),8*Time.deltaTime);
         boxshape.localScale = retractor;
         boxshape.localRotation = rotational;
 
@@ -59,6 +62,7 @@ public class soundvisualizer : MonoBehaviour
             rand = new Color32(rnd1, rnd2, rnd3,255);
 
             dottedline.color = rand;
+            backgroundeditor.color = rand;
 
             test = false;
             if (reset == true)
@@ -71,7 +75,10 @@ public class soundvisualizer : MonoBehaviour
         {
             test = true;
         }
+
+        spiral();
         backgroundorganiser();
+        
     }
 
     void backgroundselector()
@@ -88,6 +95,7 @@ public class soundvisualizer : MonoBehaviour
             sizes[i] = shapes[i].transform;
             
         }
+
         deathset = false;
         reset = false;
     }
@@ -95,7 +103,7 @@ public class soundvisualizer : MonoBehaviour
     void backgroundorganiser()
     {
        
-            for (int i = 0; i <= shapes.Count; i++)
+        for (int i = 0; i <= shapes.Count; i++)
             {
             Vector3 expand;
             if (deathset == false)
@@ -146,7 +154,52 @@ public class soundvisualizer : MonoBehaviour
             }
 
             }
+           
+
+
+
+    }
+
+    void spiral()
+    {
         
+        if (boxshape.localScale.x > 0.01 && boxshape.localScale.x < 4) ;
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                if (points.Count != 20)
+                {
+                    GameObject trangle = GameObject.Instantiate(instantiateetwo, instantiatortwo.transform);
+                    
+                    points.Add(trangle);
+                    float ang = Random.value * 360;
+                    Vector3 pos = new Vector3(instantiatortwo.transform.position.x + 5f * Mathf.Sin(ang * Mathf.Deg2Rad), instantiatortwo.transform.position.y + 5f * Mathf.Cos(ang * Mathf.Deg2Rad), instantiatortwo.transform.position.z);
+                    angleset[i] = new Vector3(Mathf.Cos(ang), Mathf.Sin(ang), 0);
+                    trangle.GetComponent<Transform>().localPosition = pos;
+                    //Quaternion rotation = Quaternion.LookRotation(trangle.GetComponent<Transform>().position, angleset);
+                    //trangle.GetComponent<Transform>().localRotation = rotation;
+                }
+                
+
+
+            }
+            for(int i = 0; i < 20; i++)
+                {
+
+                points[i].GetComponent<Transform>().localPosition =Vector3.Lerp(points[i].GetComponent<Transform>().localPosition, points[i].GetComponent<Transform>().localPosition+ angleset[i],6f*Time.deltaTime);
+               float blingo =  Vector3.Distance(points[i].GetComponent<Transform>().localPosition, instantiatortwo.transform.localPosition);
+                if (blingo > 50)
+                {
+                   
+                    GameObject x = points[i];
+                    points.RemoveAt(i);
+                    Destroy(x);
+                }
+                Debug.Log(blingo);
+                }
+        }
+
+      
     }
   
 }
